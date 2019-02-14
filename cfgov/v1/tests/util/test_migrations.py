45941739724f00f2e46f09f465ce7e818d9f7520
Scott Cranfill <scott.cranfill@cfpb.gov>
2019-02-14 10:29:18 -0500

    Merge branch 'migrate-child-stream-fields' into email-signup-standardization

--- cfgov/v1/tests/util/test_migrations.py
+++ cfgov/v1/tests/util/test_migrations.py
@@ -12,7 +12,7 @@ import mock
 from v1.tests.wagtail_pages.helpers import save_new_page
 from v1.util.migrations import (
     get_stream_data, is_page, migrate_page_types_and_fields,
-    migrate_stream_data, migrate_stream_field, set_stream_data,
+    migrate_stream_data, migrate_stream_field, set_stream_data
 )
 
 
@@ -95,14 +95,14 @@ class MigrationsUtilTestCase(TestCase):
             },
         ]
         result, migrated = migrate_stream_data(
-            self.page, 'migratory', stream_data, mapper
+            self.page, ['not-migratory', 'migratory'], stream_data, mapper
         )
         self.assertTrue(migrated)
         self.assertEquals(
             result[0]['value'][0]['value'], 'new text'
         )
 
-    def test_migrate_stream_data_no_iterable_value(self):
+    def test_migrate_stream_data_flat(self):
         mapper = mock.Mock(return_value='new text')
         stream_data = [
             {
@@ -115,12 +115,19 @@ class MigrationsUtilTestCase(TestCase):
             },
         ]
         result, migrated = migrate_stream_data(
-            self.page, 'migratory', stream_data, mapper
+            self.page, ['migratory', ], stream_data, mapper
         )
         self.assertTrue(migrated)
         self.assertEquals(
             result[1]['value'], 'new text'
         )
+
+    def test_migrate_stream_data_empty_block_path(self):
+        mapper = mock.Mock(return_value='new text')
+        result, migrated = migrate_stream_data(
+            self.page, [], {}, mapper
+        )
+        self.assertFalse(migrated)
 
     def test_migrate_stream_field_page(self):
         """ Test that the migrate_stream_field function correctly gets
@@ -195,4 +202,3 @@ class MigrationsUtilTestCase(TestCase):
                                                   'body',
                                                   'text',
                                                   mapper)
-
